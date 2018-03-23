a3f8489dec6556765dfa66a47c687000518d66ed
morshedislam <morshedislam21@gmail.com>
2018-03-24 02:20:21 +0600

    Initial commit

---
+++ app/src/main/java/com/morshed/chatapplication/model/UserDetails.java
@@ -0,0 +1,10 @@
+package com.morshed.chatapplication.model;
+
+/**
+ * Created by Morshed on 3/20/2018.
+ */
+public class UserDetails {
+    public static String username = "";
+    public static String password = "";
+    public static String chatWith = "";
+}
